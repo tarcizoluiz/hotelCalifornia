e8a909f9a1cbb606dfd3c32f506c8354a639c7a8
Tarcizo Luiz Ferreira da Costa <tarcizo.costa@mv.com.br>
2024-06-10 00:14:27 -0300

    Commit Atividade 1 Spring

---
+++ src/main/java/com/br/hotelCalifornia/HotelCaliforniaApplication.java
@@ -0,0 +1,13 @@
+package com.br.hotelCalifornia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCaliforniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/hotelCalifornia/HotelCaliforniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.hotelCalifornia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCaliforniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
